a610e438f46219cb3deff47cdd64fa9334040ed3
eimyss <eimyss@gmail.com>
2018-11-13 22:09:27 +0100

    I cannot calculate dates.

--- src/main/java/de/eimantas/eimantasbackend/helpers/DateHelper.java
+++ src/main/java/de/eimantas/eimantasbackend/helpers/DateHelper.java
@@ -10,8 +10,8 @@ import java.util.Date;
 
 public class DateHelper {
 
+  private static org.slf4j.Logger logger = LoggerFactory.getLogger(DateHelper.class);
 
-  //testing can be tricky...‚
   public static LocalDateTime getEndOfWeek(int weekNumber) {
 
     LocalDateTime desiredDateNext = LocalDateTime.now()
@@ -37,22 +37,26 @@ public class DateHelper {
    *
    * @param monthsbefore
    * @param date         to check
-   * @return
+   * @return true if the date lies in the month specified with monthsbefore.
    */
   public static boolean isInMonth(int monthsbefore, Instant date) {
-
-    // OMG So innefient
+    logger.debug("months ago: " + monthsbefore);
+    logger.debug("date to check: " + date.toString());
     LocalDate checkDate = LocalDateTime.ofInstant(date, ZoneOffset.UTC).toLocalDate();
+    logger.debug("date converted: " + checkDate);
 
     LocalDate today = LocalDate.now();
     LocalDate ago = today.minus(Period.ofMonths(monthsbefore));
 
     LocalDate start = ago.withDayOfMonth(1);
+    logger.debug("min date with number of months: " + start.toString());
     LocalDate end = ago.withDayOfMonth(ago.lengthOfMonth());
+    logger.debug("max date with number of months: " + end.toString());
 
-    return start.isBefore(checkDate) && end.isAfter(checkDate);
+    return checkDate.isAfter(start) && checkDate.isBefore(end);
 
   }
+
 
   public static String getMonthNameForAgo(int monthsBefore) {
     LocalDate today = LocalDate.now();
